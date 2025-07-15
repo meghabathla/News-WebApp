2fef39b8c9708fa96bac0908c5e478a0f488e12c
Megha <bathlamegha6@gmail.com>
2025-07-15 13:36:05 +0530

    moved config file to constant

--- src/api/config.ts
+++
@@ -1,2 +0,0 @@
-export const NEWS_API_URL = import.meta.env.VITE_NEWS_API_URL;
-export const NEWS_API_KEY = import.meta.env.VITE_NEWS_API_KEY;
---
+++ src/constants/config.ts
@@ -0,0 +1,6 @@
+//Move this env file later
+// export const NEWS_API_URL = import.meta.env.VITE_NEWS_API_URL;
+// export const NEWS_API_KEY = import.meta.env.VITE_NEWS_API_KEY;
+
+export const NEWS_API_URL = "https://newsapi.org/v2/everything";
+export const NEWS_API_KEY = "f2ef76d903c940a8ae55ae923150eb70";
